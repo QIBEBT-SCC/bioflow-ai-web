18194cf00eb09e832090aaff480b832ceccb43db
Aye10032 <tc_aye@outlook.com>
2025-03-17 23:25:48 +0800

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
 import './App.css'
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {
+    ReactFlow,
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+} from "@xyflow/react";
+import {useCallback} from "react";
 
-
+const initialNodes = [
+    {id: '1', position: {x: 0, y: 0}, data: {label: '1'}},
+    {id: '2', position: {x: 0, y: 100}, data: {label: '2'}},
+];
+const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
 
 export default function App() {
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-    )
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
+
+    return (
+        <div style={{width: '100vw', height: '100vh'}}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+            >
+                <Controls/>
+                <MiniMap/>
+                <Background variant="dots" gap={12} size={1}/>
+            </ReactFlow>
+        </div>
+    );
 }
