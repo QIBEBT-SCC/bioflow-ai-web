fb266fedb7dea72229d768ce47d63da56e08f094
Aye10032 <tc_aye@outlook.com>
2025-03-18 09:26:28 +0800

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,24 @@
-import './App.css'
-import {
-    ReactFlow,
-    MiniMap,
-    Controls,
-    Background,
-    useNodesState,
-    useEdgesState,
-    addEdge,
-} from "@xyflow/react";
-import {useCallback} from "react";
+import { useState } from 'react'
 
-const initialNodes = [
-    {id: '1', position: {x: 0, y: 0}, data: {label: '1'}},
-    {id: '2', position: {x: 0, y: 100}, data: {label: '2'}},
-];
-const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
+function App() {
+  const [count, setCount] = useState(0)
 
-export default function App() {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-    const onConnect = useCallback(
-        (params) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges],
-    );
-
-    return (
-        <div style={{width: '100vw', height: '100vh'}}>
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-            >
-                <Controls/>
-                <MiniMap/>
-                <Background variant="dots" gap={12} size={1}/>
-            </ReactFlow>
-        </div>
-    );
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
