18b400ed0b1fe321f073cc4eee6b465f0fb293e5
Aye10032 <tc_aye@outlook.com>
2025-03-27 15:14:15 +0800

    update arg type

--- src/stores/toolStore.tsx
+++ src/stores/toolStore.tsx
@@ -2,11 +2,11 @@ import {create} from 'zustand';
 import {DefaultArgs} from '@/services/api';
 
 interface ToolStore {
-    defaultArgs: DefaultArgs | null;
+    defaultArgs: DefaultArgs;
     setDefaultArgs: (args: DefaultArgs) => void;
 }
 
 export const useToolStore = create<ToolStore>((set) => ({
-    defaultArgs: null,
+    defaultArgs: {},
     setDefaultArgs: (args) => set({defaultArgs: args}),
 })); 
