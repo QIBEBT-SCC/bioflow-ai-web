76aafe8f108bb9fc80dc257c5769bf5b411c9874
Aye10032 <tc_aye@outlook.com>
2025-04-02 16:22:11 +0800

    添加字体文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import Unfonts from 'unplugin-fonts/vite'
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        Unfonts({
+            custom: {
+                families: [
+                    {
+                        name: 'Geist',
+                        src: './src/assets/fonts/geist/*.woff2',
+                    },
+                    {
+                        name: 'Geist Mono',
+                        src: './src/assets/fonts/geistmono/*.woff2',
+                    },
+                ],
+            },
+        })
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
@@ -19,7 +37,7 @@ export default defineConfig({
             // http://localhost:5173/foo
             // -> http://localhost:4567/foo
             '/api': {
-                target: 'http://172.18.19.113:8000',
+                target: 'http://172.18.19.113:8001',
                 changeOrigin: true
             },
         },
