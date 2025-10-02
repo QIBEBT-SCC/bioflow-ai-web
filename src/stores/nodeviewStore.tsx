a9bbb1338d2f0b96871ebc50e062f884548dd071
Aye10032 <tc_aye@outlook.com>
2025-10-02 16:28:09 +0800

    自定义管理节点状态

--- src/stores/nodeviewStore.tsx
+++ src/stores/nodeviewStore.tsx
@@ -1,11 +1,56 @@
+import {
+    type Edge,
+    type Node,
+    type OnNodesChange,
+    type OnEdgesChange,
+    type OnConnect,
+} from '@xyflow/react';
+import {addEdge, applyNodeChanges, applyEdgeChanges} from '@xyflow/react';
+import {devtools} from "zustand/middleware";
 import {create} from "zustand";
 
-interface NodeEditorStore {
+export interface NodeEditorStore {
     currentWorkflowUid: string;
     setCurrentWorkflowUid: (uid: string) => void;
+
+    nodes: Node[];
+    edges: Edge[];
+    onNodesChange: OnNodesChange;
+    onEdgesChange: OnEdgesChange;
+    onConnect: OnConnect;
+    setNodes: (nodes:Node[]) => void;
+    setEdges: (edges:Edge[]) => void;
 }
 
-export const useNodeEditorStore = create<NodeEditorStore>((set) => ({
-    currentWorkflowUid: '',
-    setCurrentWorkflowUid: (uid) => set({currentWorkflowUid: uid})
-}))
+export const useNodeEditorStore = create<NodeEditorStore>()(
+    devtools((set, get) => ({
+            currentWorkflowUid: "",
+            setCurrentWorkflowUid: (uid) => set({currentWorkflowUid: uid}),
+
+            nodes: [],
+            edges: [],
+            onNodesChange: (changes) => {
+                set({
+                    nodes: applyNodeChanges(changes, get().nodes),
+                });
+            },
+            onEdgesChange: (changes) => {
+                set({
+                    edges: applyEdgeChanges(changes, get().edges),
+                });
+            },
+            onConnect: (connection) => {
+                set({
+                    edges: addEdge(connection, get().edges),
+                });
+            },
+            setNodes: (nodes) => {
+                set({ nodes });
+            },
+            setEdges: (edges) => {
+                set({ edges });
+            },
+        }),
+        {name: 'node-store'}
+    )
+);
