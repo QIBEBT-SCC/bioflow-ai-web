7af5f26b29419a94922f6002c25415d9ecb1143f
Aye10032 <tc_aye@outlook.com>
2025-10-04 23:47:19 +0800

    update lint from eslint to biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from "path"
+import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
+import react from '@vitejs/plugin-react-swc'
 import Unfonts from 'unplugin-fonts/vite'
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
