349a3981623602215c129c6857d1968344fb6519
Aye10032 <tc_aye@outlook.com>
2025-10-06 02:44:30 +0800

    update hook

--- src/hooks/use-tool.tsx
+++ src/hooks/use-tool.tsx
@@ -1,130 +1,148 @@
-import {documentApi, imageApi, toolApi} from '@/services/api';
-import {useMutation, useQuery, useQueryClient, UseQueryOptions} from "@tanstack/react-query";
 import {
-    type DockerToolCreate,
-    type SimpleToolInfo,
-    type ToolInfo,
-    type ToolTag,
-    type ToolGroup,
+    type UseQueryOptions,
+    useMutation,
+    useQuery,
+    useQueryClient,
+} from '@tanstack/react-query'
+import { documentApi, imageApi, toolApi } from '@/services/api'
+import type { ToolArgPublic } from '@/types/node.tsx'
+import type {
+    DockerToolCreate,
+    SimpleToolDoc,
+    SimpleToolInfo,
+    ToolGroup,
+    ToolHelpDoc,
     ToolImage,
-    SimpleToolDoc, ToolHelpDoc
-} from "@/types/tool.tsx";
-import {ToolArgPublic} from "@/types/node.tsx";
+    ToolInfo,
+    ToolTag,
+} from '@/types/tool.tsx'
 
 export function useToolTagList() {
     const options: UseQueryOptions<ToolTag[], Error> = {
         queryKey: ['toolTagList'],
         queryFn: toolApi.getTagList,
-    };
+    }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
 export function useToolGroupList() {
     const options: UseQueryOptions<ToolGroup[], Error> = {
         queryKey: ['toolGroupList'],
         queryFn: toolApi.getGroupList,
-    };
+    }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
-export function useGroupTools({parent_id}: { parent_id?: number }) {
+export function useGroupTools({ parent_id }: { parent_id?: number }) {
     const options: UseQueryOptions<SimpleToolInfo[], Error> = {
         queryKey: ['groupTools', parent_id],
-        queryFn: ({queryKey}) => {
-            const [, parent_id] = queryKey as [string, number | undefined];
-            return toolApi.getGroupTools(parent_id);
+        queryFn: ({ queryKey }) => {
+            const [, parent_id] = queryKey as [string, number | undefined]
+            return toolApi.getGroupTools(parent_id)
         },
-    };
+    }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
 export const useCreateImage = () => {
-    const queryClient = useQueryClient();
+    const queryClient = useQueryClient()
     return useMutation({
-        mutationFn: ({image}: { image: ToolImage }) => imageApi.createImage(image),
+        mutationFn: ({ image }: { image: ToolImage }) =>
+            imageApi.createImage(image),
         onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ['allTools']}).then();
-            queryClient.invalidateQueries({queryKey: ['groupTools']}).then();
-            queryClient.invalidateQueries({queryKey: ['toolTagList']}).then();
-            queryClient.invalidateQueries({queryKey: ['searchTools']}).then();
-        }
-    });
+            queryClient.invalidateQueries({ queryKey: ['allTools'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['groupTools'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['toolTagList'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['searchTools'] }).then()
+        },
+    })
 }
 
-export const useSearchImages = ({name}: { name: string }) => {
+export const useSearchImages = ({ name }: { name: string }) => {
     const options: UseQueryOptions<ToolImage[], Error> = {
         queryKey: ['images', name],
-        queryFn: ({queryKey}) => {
-            const [, name] = queryKey as [string, string];
-            return imageApi.searchImages(name);
+        queryFn: ({ queryKey }) => {
+            const [, name] = queryKey as [string, string]
+            return imageApi.searchImages(name)
         },
         enabled: !!name,
     }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
-export const useImageDocuments = ({uid}: { uid: string }) => {
+export const useImageDocuments = ({ uid }: { uid: string }) => {
     const options: UseQueryOptions<SimpleToolDoc[], Error> = {
         queryKey: ['imageDocs', uid],
-        queryFn: ({queryKey}) => {
-            const [, uid] = queryKey as [string, string];
-            return imageApi.getImageDocs(uid);
+        queryFn: ({ queryKey }) => {
+            const [, uid] = queryKey as [string, string]
+            return imageApi.getImageDocs(uid)
         },
         enabled: !!uid,
     }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
 export const useRunInImage = () => {
     return useMutation({
-        mutationFn: ({uid, command}: { uid: string, command: string }) =>
+        mutationFn: ({ uid, command }: { uid: string; command: string }) =>
             imageApi.runInImage(uid, command),
-    });
+    })
 }
 
-export const useDocument = ({uid}: { uid: string }) => {
+export const useDocument = ({ uid }: { uid: string }) => {
     const options: UseQueryOptions<ToolHelpDoc, Error> = {
         queryKey: ['documents', uid],
-        queryFn: ({queryKey}) => {
-            const [, uid] = queryKey as [string, string];
-            return documentApi.getDocument(uid);
+        queryFn: ({ queryKey }) => {
+            const [, uid] = queryKey as [string, string]
+            return documentApi.getDocument(uid)
         },
         enabled: !!uid,
     }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
-export function useSearchTools({name, offset}: { name: string, offset?: number }) {
+export function useSearchTools({
+    name,
+    offset,
+}: {
+    name: string
+    offset?: number
+}) {
     const options: UseQueryOptions<SimpleToolInfo[], Error> = {
         queryKey: ['searchTools', name, offset],
-        queryFn: ({queryKey}) => {
-            const [, name, offset] = queryKey as [string, string, number | undefined];
-            return toolApi.searchToolList(name, offset);
+        queryFn: ({ queryKey }) => {
+            const [, name, offset] = queryKey as [
+                string,
+                string,
+                number | undefined,
+            ]
+            return toolApi.searchToolList(name, offset)
         },
         enabled: !!name,
     }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
 export function useCreateTool() {
-    const queryClient = useQueryClient();
+    const queryClient = useQueryClient()
     return useMutation({
-        mutationFn: ({tool}: { tool: DockerToolCreate }) => toolApi.newTool(tool),
+        mutationFn: ({ tool }: { tool: DockerToolCreate }) =>
+            toolApi.newTool(tool),
         onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ['allTools']}).then();
-            queryClient.invalidateQueries({queryKey: ['documents']}).then();
-            queryClient.invalidateQueries({queryKey: ['groupTools']}).then();
-            queryClient.invalidateQueries({queryKey: ['toolTagList']}).then();
-            queryClient.invalidateQueries({queryKey: ['searchTools']}).then();
-        }
-    });
+            queryClient.invalidateQueries({ queryKey: ['allTools'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['documents'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['groupTools'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['toolTagList'] }).then()
+            queryClient.invalidateQueries({ queryKey: ['searchTools'] }).then()
+        },
+    })
 }
 
 export function useToolCount() {
@@ -133,41 +151,42 @@ export function useToolCount() {
         queryFn: toolApi.getToolCount,
     }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
 export function useAllTools(offset: number) {
     const options: UseQueryOptions<SimpleToolInfo[], Error> = {
         queryKey: ['allTools', offset],
-        queryFn: ({queryKey}) => {
-            const [, offset] = queryKey as [string, number];
-            return toolApi.getToolList(offset);
-        }
-    };
+        queryFn: ({ queryKey }) => {
+            const [, offset] = queryKey as [string, number]
+            return toolApi.getToolList(offset)
+        },
+    }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
-export function useTool({uid}: { uid: string }) {
+export function useTool({ uid }: { uid: string }) {
     const options: UseQueryOptions<ToolInfo, Error> = {
         queryKey: ['tool', uid],
-        queryFn: ({queryKey}) => {
-            const [, uid] = queryKey as [string, string];
-            return toolApi.getTool(uid);
+        queryFn: ({ queryKey }) => {
+            const [, uid] = queryKey as [string, string]
+            return toolApi.getTool(uid)
         },
-    };
+    }
 
-    return useQuery(options);
+    return useQuery(options)
 }
 
-export function useToolArg({uid}: { uid: string }) {
+export function useToolArg({ uid }: { uid: string }) {
     const options: UseQueryOptions<ToolArgPublic, Error> = {
         queryKey: ['toolArg', uid],
-        queryFn: ({queryKey}) => {
-            const [, uid] = queryKey as [string, string];
-            return toolApi.getToolArg(uid);
+        queryFn: ({ queryKey }) => {
+            const [, uid] = queryKey as [string, string]
+            return toolApi.getToolArg(uid)
         },
-    };
+        enabled: !!uid, // 只有当uid不为空时才执行查询
+    }
 
-    return useQuery(options);
+    return useQuery(options)
 }
