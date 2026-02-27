d487f4731dd67be6aa588de207cbe8c61eafc68c
Aye10032 <tc_aye@outlook.com>
2026-02-27 16:46:15 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: `${process.env.BACKEND_API_URL || 'http://localhost:8000'}/:path*`,
+        destination: `${process.env.BACKEND_API_URL || 'http://localhost:8000/api/v1'}/:path*`,
       },
     ]
   },
