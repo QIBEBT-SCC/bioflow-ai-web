7f2edf1766c63fcf106dd9766988686ce233390c
Aye10032 <tc_aye@outlook.com>
2026-03-30 16:42:49 +0800

    fix(api): 204 No Content

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -76,6 +76,11 @@ export async function clientFetch<T = unknown>(
     )
   }
 
+  // 204 No Content 没有响应体
+  if (res.status === 204) {
+    return undefined as T
+  }
+
   const contentType = res.headers.get('content-type')
   if (contentType?.includes('application/json')) {
     return res.json()
