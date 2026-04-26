e7e2e07c5cd6f5d14f96fb1157be98b6e31391c2
Aye10032 <tc_aye@outlook.com>
2026-04-27 00:19:31 +0800

    style: update

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -29,7 +29,7 @@ function SidebarLogo() {
         alt='BioFlow AI'
         width={collapsed ? 32 : 140}
         height={32}
-        className="w-[95%] h-auto"
+        className='w-[95%] h-auto'
         priority
       />
     </div>
